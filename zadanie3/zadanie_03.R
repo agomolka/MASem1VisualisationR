1199e90c813beae5dbbcf88f58906e36529be7ac
Aleksandra Gomolka <agomolka@pm.me>
2022-12-30 09:58:16 +0100

    Added "Date" string to report

--- zadanie3/zadanie_03.R
+++ zadanie3/zadanie_03.R
@@ -61,3 +61,75 @@ print(rbind(x,x,x,x))
 
 # -- bedzie zawieral tabele z danymi. Tabela powinna miec kolumny: Timestamp;Wiatr;PV; 
 #     Tabelka ma zawierac sie w osobnym rozdziale raportu.
+
+#---------------------
+library(ggplot2)
+library(reshape2)
+
+dates <- lapply(gsub("-","",as.character(seq(from=Sys.Date()-6,to=Sys.Date(),by="1 day"))),function(d){d})
+
+download.file(url=paste("https://www.pse.pl/getcsv/-/export/csv/PL_GEN_WIATR/data/", dates[1][1], sep=""),
+              destfile=paste(dates[1][1],".csv",sep=""), method="curl")
+d <- read.table(file=paste( dates[1][1], ".csv", sep=""), sep=";",dec=",",header=T,fileEncoding="CP1250")
+
+for(date in dates[c(2:length(dates))]){
+  download.file(url=paste("https://www.pse.pl/getcsv/-/export/csv/PL_GEN_WIATR/data/",date[1],sep=""),
+                destfile=paste(date[1],".csv",sep=""), method="curl")
+  
+  d_temp <- read.table(file=paste(date[1], ".csv", sep=""), sep=";",dec=",",header=T,fileEncoding="CP1250")
+  d <- rbind(d, setNames(d_temp, names(d_temp)))
+}
+
+d$Godzina <- as.integer(d$Godzina)
+d$Timestamp <- as.POSIXct(paste0(d$Data," ",d$Godzina-1,":00:00"),tz="GMT")
+
+d <- d[order(d$Timestamp),]
+
+#plot(x=d$Timestamp,y=d$Generacja.źródeł.wiatrowych,xlab="",ylab="",main="eee")
+#lines(x=d$Timestamp,y=d$Generacja.źródeł.fotowoltaicznych,col="red")
+
+d.long <- melt(d, id = "Timestamp", measure = c("Generacja.źródeł.fotowoltaicznych", "Generacja.źródeł.wiatrowych"))
+#ggplot(d.long, aes(Timestamp, value, colour = variable)) + geom_line()
+
+img <- ( 
+  ggplot(d.long, aes(Timestamp, value, colour = variable))
+  + 
+    xlab("Czas") 
+  +
+    scale_y_continuous("Generacja.źródeł.fotowoltaicznych",
+                       sec.axis = sec_axis(~ . * 30.48,name="Generacja.źródeł.wiatrowych"))
+  +
+    geom_line() 
+  + 
+    geom_point() 
+)
+print(img)
+
+#-- miko
+rm(list=ls())
+#Import required libraries
+library(ggplot2)
+
+#Download data for last 7 days
+arr_dest <- c()
+arr<-lapply(gsub("-","",as.character(seq(from=Sys.Date()-6,to=Sys.Date(),by="1 day"))),function(d){
+  url_basic <- "https://www.pse.pl/getcsv/-/export/csv/PL_GEN_WIATR/data/20221210"
+  urls <- gsub(substr(url_basic,58,65),d, url_basic)
+  destinations <- paste(d, ".csv", sep="")
+  download.file(url=urls,destfile=destinations,method="curl")
+  append(arr_dest, destinations)
+})
+
+#Union all dataframes in one
+df_init <-read.table(file=arr[[1]],sep=";",dec=",",header=T,fileEncoding="CP1250")
+arr_tmp <- arr[2:7]
+for(i in arr_tmp){
+  df_increment <- read.table(file=i,sep=";",dec=",",header=T,fileEncoding="CP1250")
+  df_init<-merge(df_init,df_increment,all=TRUE)
+}
+#Prepare columns (types,format,names)
+df_init$Godzina<-paste(df_init$Godzina, "00","00", sep=":")
+df_init$Data <- as.Date(df_init$Data, format="%Y-%m-%d")
+df_init$Timestamp <- as.POSIXct(paste(df_init$Data, df_init$Godzina), format="%Y-%m-%d %H:%M:%S")
+names(df_init) <- c("Data", "Godzina", "Wiatr", "PV", "Timestamp")
+
